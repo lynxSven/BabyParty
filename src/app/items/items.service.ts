fb527c6fb395771ad49129af21a18fc69b6f5124
DESKTOP-8HQ5MG6\Sven <sven.ole.lux1991@gmail.com>
2023-07-04 19:14:58 +0200

    added new endpoint null checks

--- src/app/items/items.service.ts
+++ src/app/items/items.service.ts
@@ -15,13 +15,13 @@ export class ItemsService {
 
 
   loadDataReservedByCurrentUser(): void {
-    this.itemsCurrentUser = [
-      {
-        headline: "headline",
-        href: "https://babypartybackend.adaptable.app/todos",
-        hrefImg: "https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, cumque ut deserunt aspernatur libero quibusdam earum sequi accusamus impedit vel obcaecati suscipit quae, exercitationem, eaque quas. Debitis accusantium in nostrum sapiente est velit dolor eius ipsam amet et? Rem natus error dicta mollitia doloribus. Voluptas blanditiis sequi minima aliquam cumque?"
-      }]
+
+    this.http.get<any>("https://babypartybackend.adaptable.app/shopping-list/currentUser").pipe(retry(1), catchError(this.handleError)).subscribe(
+      (data: Items[]) => {
+        console.log("got my data ", data);
+        this.itemsCurrentUser = data;
+      }
+    );
   }
 
   loadAllData(): void {
