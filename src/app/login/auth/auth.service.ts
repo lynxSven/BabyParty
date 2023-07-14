75ce1ed66b0caceea8a65b8b33332d6fb4367e29
DESKTOP-8HQ5MG6\Sven <sven.ole.lux1991@gmail.com>
2023-07-14 18:07:51 +0200

    user service img width

--- src/app/login/auth/auth.service.ts
+++ src/app/login/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import { catchError, retry, throwError } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,13 +10,51 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   currentUser = 'someone';
+  allUsers: User[] = []
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   isAuthenticated(): boolean {
-    if(this.currentUser !== undefined && this.currentUser !== null){
+    if (this.currentUser !== undefined && this.currentUser !== null) {
       return false;
     }
     return false;
+  }
+
+  loadAllData(): void {
+
+    this.http.get<any>("https://babypartybackend.adaptable.app/user").pipe(retry(1), catchError(this.handleError)).subscribe(
+      (data: User[]) => {
+        console.log("got my data ", data);
+        this.allUsers = data;
+      }
+    );
+  }
+
+  // loadAllData(): void {
+
+  //   this.http.get<any>("https://babypartybackend.adaptable.app/shopping-list").pipe(retry(1), catchError(this.handleError)).subscribe(
+  //     (data: Items[]) => {
+  //       console.log("got my data ", data);
+  //       this.allUsers = data;
+  //     }
+  //   );
+  // }
+  // // Error handling
+  handleError(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    // window.alert(errorMessage);
+    console.log(errorMessage);
+    return throwError(() => {
+      return errorMessage;
+    });
+    // }
   }
 }
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  constructor(public authService: AuthService){
+
+  }
+  ngOnInit() {
+    this.authService.loadAllData(); 
+  }
+
 
 }
